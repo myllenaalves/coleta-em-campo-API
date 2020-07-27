cb2328cc7b2047933aeaca1b43e151332bee85eb
myllenaalves <mal4@cin.ufpe.br>
2020-07-27 19:30:22 -0300

    Alterando configurações da autenticação jwt

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,15 +1,38 @@
-class JsonWebToken
- class << self
-   def encode(payload, exp = 24.hours.from_now)
-     payload[:exp] = exp.to_i
-     JWT.encode(payload, Rails.application.secrets.secret_key_base)
-   end
+require 'jwt'
 
-   def decode(token)
-     body = JWT.decode(token, Rails.application.secrets.secret_key_base)[0]
-     HashWithIndifferentAccess.new body
-   rescue
-     nil
-   end
- end
-end
+class JsonWebToken
+  # Encodes and signs the payload (e.g. the user email) using our app's secret key
+  # The result also includes the expiration date.
+  def self.encode(payload)
+    payload.reverse_merge!(meta)
+    JWT.encode(payload, Rails.application.secrets.secret_key_base)
+  end
+
+  # Decode the JWT to get the user email
+  def self.decode(token)
+    JWT.decode(token, Rails.application.secrets.secret_key_base)
+  end
+
+  # Validates the payload hash for expiration and meta claims
+  def self.valid_payload(payload)
+    if expired(payload) || payload['iss'] != meta[:iss] || payload['aud'] != meta[:aud]
+      return false
+    else
+      return true
+    end
+  end
+
+  # Default options to be encoded in the token
+  def self.meta
+    {
+      exp: 7.days.from_now.to_i,
+      iss: 'issuer_name',
+      aud: 'client',
+    }
+  end
+
+  # Validates if the token is expired by exp parameter
+  def self.expired(payload)
+    Time.at(payload['exp']) < Time.now
+  end
+end 
