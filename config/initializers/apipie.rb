955d8fe4665271ef9e117445fe075283f915d581
myllenaalves <mal4@cin.ufpe.br>
2020-07-27 17:39:24 -0300

    Atualizando tradução do apipie

--- config/initializers/apipie.rb
+++ config/initializers/apipie.rb
@@ -1,7 +1,8 @@
 Apipie.configure do |config|
-  config.app_name                = "JwtRailsApiApp"
+  config.app_name                = "Coleta de dados API"
   config.api_base_url            = "/api"
   config.doc_base_url            = "/apipie"
   # where is your API defined?
   config.api_controllers_matcher = "#{Rails.root}/app/controllers/**/*.rb"
+  config.translate = false
 end
